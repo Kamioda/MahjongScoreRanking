9a58d427f0fabab08e02bb9fed612b3894645f3d
Kamioda <aimegu2014@gmail.com>
2023-08-13 18:02:57 +0900

    テスト名の修正とasync、awaitの削除

--- APIServer/tests/database/Account.js
+++ APIServer/tests/database/Account.js
@@ -4,12 +4,14 @@ import assert from 'assert';
 describe('Account Manager Test', function () {
     const AccountMgr = new AccountManager();
     describe('add', function () {
-        it('test', async function () {
-            await AccountMgr.AddNewAccount('kamioda_ampsprg', '神御田', 0).then(data => {
+        it('test/admin', function () {
+            AccountMgr.AddNewAccount('kamioda_ampsprg', '神御田', 0).then(data => {
                 assert.equal(data.id, 'kamioda_ampsprg');
                 assert.equal(data.password.length, 8);
             });
-            await AccountMgr.AddNewAccount('ayaka_meigetsu', '明月彩香', 1).then(data => {
+        });
+        it('test/user', function() {
+            AccountMgr.AddNewAccount('ayaka_meigetsu', '明月彩香', 1).then(data => {
                 assert.equal(data.id, 'ayaka_meigetsu');
                 assert.equal(data.password.length, 8);
             });
