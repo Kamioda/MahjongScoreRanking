de6638af207f360626702eb8734b24f3bb70c042
Kamioda <aimegu2014@gmail.com>
2023-08-13 16:51:30 +0900

    レコード削除のテストを追加

--- APIServer/tests/others/ScoreManager.js
+++ APIServer/tests/others/ScoreManager.js
@@ -90,4 +90,65 @@ describe('ScoreManager Test', function() {
             unlinkSync(RecordFile);
         });
     });
+    describe('remove', function() {
+        const TestBeforeRemoveRecordData = {
+            kamioda_ampsprg: [
+                { id: '0ec8171c88c94b4e9df484df302a90dd', date: testDate, score: 25000 },
+                { id: 'e0c4e6db48884b338b5f069099958a74', date: testDate, score: 17000 },
+                { id: '72c9f897ece34c46852ea1882cba8790', date: testDate, score: 23000 },
+                { id: 'f321a0644733488c9a307ff1d9950a1e', date: testDate, score: 40000 },
+                { id: 'b305e055212d45a08e0d4b0491543f5f', date: testDate, score: 1000 },
+            ],
+            ayaka_meigetsu: [
+                { id: '0ec8171c88c94b4e9df484df302a90dd', date: testDate, score: 40000 },
+                { id: 'e0c4e6db48884b338b5f069099958a74', date: testDate, score: 90000 },
+                { id: '72c9f897ece34c46852ea1882cba8790', date: testDate, score: 0 },
+                { id: 'f321a0644733488c9a307ff1d9950a1e', date: testDate, score: 120000 },
+                { id: 'b305e055212d45a08e0d4b0491543f5f', date: testDate, score: 104000 },
+            ],
+            mirai_amairo: [
+                { id: '0ec8171c88c94b4e9df484df302a90dd', date: testDate, score: 40000 },
+                { id: 'e0c4e6db48884b338b5f069099958a74', date: testDate, score: -2000 },
+                { id: '72c9f897ece34c46852ea1882cba8790', date: testDate, score: 82000 },
+                { id: 'f321a0644733488c9a307ff1d9950a1e', date: testDate, score: -55000 }, 
+            ],
+            amairo_miyuki: [
+                { id: 'b305e055212d45a08e0d4b0491543f5f', date: testDate, score: 0 }
+            ]
+        };
+        const TestAfterRemoveRecordData = {
+            kamioda_ampsprg: [
+                { id: '0ec8171c88c94b4e9df484df302a90dd', date: testDate, score: 25000 },
+                { id: 'e0c4e6db48884b338b5f069099958a74', date: testDate, score: 17000 },
+                { id: 'f321a0644733488c9a307ff1d9950a1e', date: testDate, score: 40000 },
+                { id: 'b305e055212d45a08e0d4b0491543f5f', date: testDate, score: 1000 },
+            ],
+            ayaka_meigetsu: [
+                { id: '0ec8171c88c94b4e9df484df302a90dd', date: testDate, score: 40000 },
+                { id: 'e0c4e6db48884b338b5f069099958a74', date: testDate, score: 90000 },
+                { id: 'f321a0644733488c9a307ff1d9950a1e', date: testDate, score: 120000 },
+                { id: 'b305e055212d45a08e0d4b0491543f5f', date: testDate, score: 104000 },
+            ],
+            mirai_amairo: [
+                { id: '0ec8171c88c94b4e9df484df302a90dd', date: testDate, score: 40000 },
+                { id: 'e0c4e6db48884b338b5f069099958a74', date: testDate, score: -2000 },
+                { id: 'f321a0644733488c9a307ff1d9950a1e', date: testDate, score: -55000 }, 
+            ],
+            amairo_miyuki: [
+                { id: 'b305e055212d45a08e0d4b0491543f5f', date: testDate, score: 0 }
+            ]
+        };
+        before(function() {
+            if (existsSync(RecordFile)) unlinkSync(RecordFile);
+            writeFileSync(RecordFile, JSON.stringify(TestBeforeRemoveRecordData));
+        });
+        after(function(){
+            unlinkSync(RecordFile);
+        });
+        it('test', function() {
+            const ScoreMgr = new ScoreManager(RecordFile);
+            ScoreMgr.remove('72c9f897ece34c46852ea1882cba8790');
+            assert.deepEqual(ScoreMgr.read(), TestAfterRemoveRecordData);
+        });
+    });
 });
