e95a836ddba3fefa09dae458fb5b791637b1bb45
Kamioda <aimegu2014@gmail.com>
2023-08-13 18:22:34 +0900

    仮パスワード生成をモックし、戻り値検査を修正

--- APIServer/tests/database/Account.js
+++ APIServer/tests/database/Account.js
@@ -1,5 +1,7 @@
 import AccountManager from '../../features/Account.js';
 import assert from 'assert';
+import * as sinon from 'sinon';
+import { expect } from 'expect';
 
 class AccountManagerForTest {
     constructor(AccountManagerInstance) {
@@ -38,21 +40,36 @@ class AccountManagerForTest {
 }
 
 describe('Account Manager Test', function () {
-    const AccountMgr = new AccountManagerForTest(new AccountManager());
+    const PrePasswordForTest = 'password01';
     describe('add', function () {
+        const AccountMgr = new AccountManagerForTest(new AccountManager());
+        let stubCreatePrePassword = null;
+        before(function() {
+            stubCreatePrePassword = sinon.stub(AccountMgr.AMI, 'createPrePassword').callsFake(len => {
+                expect(len).toBeGreaterThan(1);
+                return PrePasswordForTest;
+            });
+        });
         after(function() {
             AccountMgr.DeleteAllAccount();
+            if (stubCreatePrePassword && stubCreatePrePassword.restore) stubCreatePrePassword.restore();
         });
         it('test/admin', function () {
+            const expected = {
+                id: 'kamioda_ampsprg',
+                password: PrePasswordForTest
+            };
             AccountMgr.AddNewAccount('kamioda_ampsprg', '神御田', 0).then(data => {
-                assert.equal(data.id, 'kamioda_ampsprg');
-                assert.equal(data.password.length, 8);
+                assert.deepEqual(data, expected);
             });
         });
         it('test/user', function() {
+            const expected = {
+                id: 'ayaka_meigetsu',
+                password: PrePasswordForTest
+            };
             AccountMgr.AddNewAccount('ayaka_meigetsu', '明月彩香', 1).then(data => {
-                assert.equal(data.id, 'ayaka_meigetsu');
-                assert.equal(data.password.length, 8);
+                assert.deepEqual(data, expected);
             });
         });
     });
