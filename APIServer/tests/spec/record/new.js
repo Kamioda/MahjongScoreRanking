3f7b2971f2109b98b16121c8e0de6969e60ee5c3
Kamioda <aimegu2014@gmail.com>
2023-08-13 15:23:10 +0900

    テストパターンとコードを追加

--- APIServer/tests/spec/record/new.js
+++ APIServer/tests/spec/record/new.js
@@ -3,15 +3,53 @@ import { newRecordSpec } from '../../../features/specs/record/new.js';
 import assert from 'assert';
 const { isValid } = pkg;
 
-const correctPattern = {
-    kamioda: 25000,
-    KAMIODA: 25000,
-    kamioda_ampsprg: 25000,
-    KamiodaAmpsprg: 25000
-};
+const correctPattern = [
+    {
+        kamioda: 25000,
+        KAMIODA: 25000,
+        kamioda_ampsprg: 25000,
+        KamiodaAmpsprg: 25000
+    },
+    {
+        kamioda_ampsprg: 25000,
+        KamiodaAmpsprg: 25000,
+        Kamioda_Ampsprg: 25000
+    },
+    {
+        Kamioda_Ampsprg: 25000,
+        KAMIODA_AMPSPRG: 25000
+    },
+];
+
+const ngPattern = {
+    overPlayer: {
+        kamioda: 25000,
+        KAMIODA: 25000,
+        kamioda_ampsprg: 25000,
+        KamiodaAmpsprg: 25000,
+        Kamioda_Ampsprg: 25000
+    },
+    noFriend: {
+        KAMIODA_AMPSPRG: 25000
+    },
+    empty: {}
+}
 
 describe('New Record Add Request Body', function() {
     it('valid', function() {
-        assert.equal(isValid(newRecordSpec, correctPattern), true);
+        correctPattern.forEach(i => {
+            assert.equal(isValid(newRecordSpec, i), true);
+        })
+    });
+    describe('invalid', function() {
+        it('over player', function() {
+            assert.equal(isValid(newRecordSpec, ngPattern.overPlayer), false);
+        });
+        it('no friend', function() {
+            assert.equal(isValid(newRecordSpec, ngPattern.noFriend), false);
+        });
+        it('empty', function() {
+            assert.equal(isValid(newRecordSpec, ngPattern.empty), false);
+        });
     });
 }); 
