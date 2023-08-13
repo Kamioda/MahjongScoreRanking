933f15fe0b58472f6deb4ff866665eeea6951f9f
Kamioda <aimegu2014@gmail.com>
2023-08-13 18:09:47 +0900

    テストしやすいようにアカウントマネージャーをラップし、データベースをクリアするコードを追加

--- APIServer/tests/database/Account.js
+++ APIServer/tests/database/Account.js
@@ -1,9 +1,48 @@
 import AccountManager from '../../features/Account.js';
 import assert from 'assert';
 
+class AccountManagerForTest {
+    constructor(AccountManagerInstance) {
+        /**
+         * @type {AccountManager}
+         */
+        this.AMI = AccountManagerInstance;
+    }
+    async AddNewAccount(PreID, Name, Level) {
+        return await this.AMI.AddNewAccount(PreID, Name, Level);
+    }
+    async ChangePassword(ID, NewPassword) {
+        return await this.AMI.ChangePassword(ID, NewPassword);
+    }
+    async ChangeUserInfo(ID, NewRecord) {
+        return await this.AMI.ChangeUserInfo(ID, NewRecord);
+    }
+    async ChangePrivilege(ID, NewPriv) {
+        return await this.AMI.ChangePrivilege(ID, NewPriv);
+    }
+    async GetAccountInfo(ID) {
+        return await this.AMI.GetAccountInfo(ID);
+    }
+    async SignIn(ID, Password) {
+        return await this.AMI.SignIn(ID, Password);
+    }
+    async DeleteUser(ID) {
+        return await this.AMI.DeleteUser(ID);
+    }
+    async GetAccountCount() {
+        return await this.AMI.GetAccountCount();
+    }
+    async DeleteAllAccount() {
+        this.AMI.Client.accounts.deleteMany();
+    }
+}
+
 describe('Account Manager Test', function () {
-    const AccountMgr = new AccountManager();
+    const AccountMgr = new AccountManagerForTest(new AccountManager());
     describe('add', function () {
+        after(function() {
+            AccountMgr.DeleteAllAccount();
+        });
         it('test/admin', function () {
             AccountMgr.AddNewAccount('kamioda_ampsprg', '神御田', 0).then(data => {
                 assert.equal(data.id, 'kamioda_ampsprg');
