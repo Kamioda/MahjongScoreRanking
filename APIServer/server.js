8935b00cc3f626335fdf6d8ebb41819a11b3d5e1
Kamioda <aimegu2014@gmail.com>
2023-08-16 18:03:12 +0900

    サインインのエンドポイントを追加

--- APIServer/server.js
+++ APIServer/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pkg from '@json-spec/core';
 import { createAccountSpec } from './features/specs/account/create.js';
 import { AccountManager } from './features/Account.js';
+import { signinSpec } from './features/specs/account/signin.js';
 const { isValid } = pkg;
 const AccountMgr = new AccountManager();
 
@@ -14,5 +15,15 @@ export const server = () => {
             .catch(er => {
                 console.log(er.message);
             });
+    });
+    app.post('/api/signin', express.json(), async (req, res) => {
+        if (!isValid(signinSpec, req.body)) return res.sendStatus(400);
+        await AccountMgr.SignIn(req.body.id, req.body.password)
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch(() => {
+            res.sendStatus(401);
+        });
     });
 };
