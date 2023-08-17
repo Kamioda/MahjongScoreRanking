c89449fa22a44d2a2b1cad77483e6efd97a54481
Kamioda <aimegu2014@gmail.com>
2023-08-17 15:17:09 +0900

    ファイル名を変更

--- APIServer/tests/others/Account.ts
+++ APIServer/tests/others/Account.ts
@@ -4,7 +4,7 @@ import assert from 'assert';
 
 describe('Hash password test', function () {
     it('test', function () {
-        assert.equal(
+        assert.strictEqual(
             HashPassword('password01'),
             'e0638591f28a1a0da8308f9a8413d34b6b6568dc6db1e3d952dfba0fcdd0452216473232c78441ab5c4812e602a5a294adc4275416e2666fe43081009547e4c6'
         );
